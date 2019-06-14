325c8258f167c7619eb0229a6c162b393542752e
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2019-06-14 10:27:13 +0530

    Update ascen1change.py

--- ascen1change.py
+++ ascen1change.py
@@ -1,6 +1,14 @@
-p=int(input())
-q=list(map(int,input().split()))
-for i in range (0,p-1):
-    if(q[i]!=i+1):
-        print(i)
-        break
+o=input()
+j=int(o)
+num=input().split()
+s1=[]
+s2=[]
+for i in num:
+  s1.append(int(i))
+  s2.append(int(i))
+s2.sort()
+for i in range(j):
+  if s1[i]!=s2[i]:
+    c=i
+    break
+print(c)
